f7eefa16d1828c7653d3550263895c49d2f71bb7
greggall <gregoryngall@gmail.com>
2020-05-22 11:01:24 -0400

    contact page copy update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,39 +4,37 @@ import Layout from "../components/layout"
 import Header from "../components/header"
 import { Helmet } from "react-helmet"
 
-
 const ContactPage = ({ data }) => {
   return (
-
     <Layout>
-
       <div className="item contact-hero-wrapper">
-          <div className="contact-hero-copy mobile-padding contact-wrapper">
-            <h1>Let's Do<span className="purple-copy"> This</span>.</h1>
+        <div className="contact-hero-copy mobile-padding contact-wrapper">
+          <h1>
+            Let's Do<span className="purple-copy"> This</span>.
+          </h1>
 
-            <p>To kick things off, please take two minutes (I promise that’s all it will take) to answer the questions below. This information will help me determine the best way to achieve your goals of world domination.</p>
-            <p>I'll follow up within two business days. Can't wait to chat more!</p>
-            
-          </div>
+          <p>
+            To kick things off, please take two minutes to answer the questions
+            below.
+          </p>
+          <p>
+            I'll follow up within two business days. Can't wait to chat more!
+          </p>
+        </div>
       </div>
-          
+
       <Helmet>
-
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.14/iframeResizer.min.js"></script>
-
+        <script
+          type="text/javascript"
+          src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.14/iframeResizer.min.js"
+        ></script>
       </Helmet>
-            
-           
-            <div className="contact-form">
-              
-              <iframe src="https://hello.dubsado.com:443/public/form/view/5e18beecaf35f127cc612fb3"></iframe>
 
-            </div>
-          
-
+      <div className="contact-form">
+        <iframe src="https://hello.dubsado.com:443/public/form/view/5e18beecaf35f127cc612fb3"></iframe>
+      </div>
     </Layout>
-    )
-  
-  }
+  )
+}
 
-  export default ContactPage
+export default ContactPage
